b2880b8655ab71b7a73ff8d00e5a6a2e4e8f9fce
chuchengzhi <livecityccz@163.com>
2025-07-13 07:56:57 +0800

    init commit

---
+++ chatmodel-example/src/main/java/com/alibaba/cloud/ai/example/ChatModelExampleApplication.java
@@ -0,0 +1,13 @@
+package com.alibaba.cloud.ai.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatModelExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatModelExampleApplication.class, args);
+	}
+
+}
